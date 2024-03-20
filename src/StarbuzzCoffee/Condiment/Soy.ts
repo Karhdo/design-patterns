9faf57c3f8d9d6bc947e779949eb724beb2f25c8
khanhdt <khanhdt@younetgroup.com>
2024-03-20 15:24:52 +0700

    Observer Pattern

--- src/StarbuzzCoffee/Condiment/Soy.ts
+++ src/StarbuzzCoffee/Condiment/Soy.ts
@@ -1,4 +1,4 @@
-import Beverage from '../Beverage';
+import Beverage, { Size } from '../Beverage';
 import CondimentDecorator from '../CondimentDecorator';
 
 export default class Soy extends CondimentDecorator {
@@ -13,6 +13,18 @@ export default class Soy extends CondimentDecorator {
   }
 
   public cost(): number {
-    return this.beverage.cost() + 0.15;
+    let cost = this.beverage.cost();
+
+    const beverageSize = this.beverage.getSize();
+
+    if (beverageSize === Size.TALL) {
+      cost += 0.1;
+    } else if (beverageSize === Size.GRANDE) {
+      cost += 0.15;
+    } else if (beverageSize === Size.VENTI) {
+      cost += 0.2;
+    }
+
+    return cost;
   }
 }
